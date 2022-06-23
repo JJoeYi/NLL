7afc185616d52d835028983ae19b40027a190aa9
JJoeYi <joelowyi@gmail.com>
2022-06-23 21:44:26 +0800

    added text + some colours

--- app/src/main/java/com/orbitalnll/nll/StrengthMiniFragment.java
+++ app/src/main/java/com/orbitalnll/nll/StrengthMiniFragment.java
@@ -1,11 +1,25 @@
 package com.orbitalnll.nll;
 
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class StrengthMiniFragment extends Fragment {
 
     public StrengthMiniFragment() {
         super(R.layout.fragment_strength_mini);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView text = view.findViewById(R.id.stregnth_mini_textView);
+        text.setMovementMethod(new ScrollingMovementMethod());
     }
 
 }
