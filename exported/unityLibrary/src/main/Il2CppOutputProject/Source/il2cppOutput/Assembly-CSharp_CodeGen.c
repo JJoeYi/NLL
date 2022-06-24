94a53a0a1d66d8d42db0a8452bce38b601d97a0a
JJoeYi <joelowyi@gmail.com>
2022-06-24 19:54:37 +0800

    embedded temporary Unity game

---
+++ exported/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,229 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void DeadCanvas_Manager::SetScoreValues(System.Int32,System.Int32)
+extern void DeadCanvas_Manager_SetScoreValues_m5EADA471067A78758E39D0FCC1D538552B621700 (void);
+// 0x00000002 System.Void DeadCanvas_Manager::.ctor()
+extern void DeadCanvas_Manager__ctor_m18561F35BDB84ACE19FD979A1BD4E1BDC5B7D781 (void);
+// 0x00000003 System.Boolean FlappyBirdController::get_IsDead()
+extern void FlappyBirdController_get_IsDead_m674AE80FE8EBC7894231F02A0463120DAB9AE64D (void);
+// 0x00000004 System.Void FlappyBirdController::set_IsDead(System.Boolean)
+extern void FlappyBirdController_set_IsDead_m759BC84B979C4235D4BDB8F9FB68C277F30AB847 (void);
+// 0x00000005 System.Void FlappyBirdController::Start()
+extern void FlappyBirdController_Start_mFEE39692CA8EBDF3572319E6AE9EF975010BFC23 (void);
+// 0x00000006 System.Void FlappyBirdController::Update()
+extern void FlappyBirdController_Update_m45F9A45A2010AE6AC8C4FB4B9020F2F8F8FCDE70 (void);
+// 0x00000007 System.Void FlappyBirdController::Flap()
+extern void FlappyBirdController_Flap_m057D63348E1809B9DBF82942FEC89F628F60560E (void);
+// 0x00000008 System.Void FlappyBirdController::MoveCameraForward()
+extern void FlappyBirdController_MoveCameraForward_m7F064934739EDF67DF1C1038D5E80B1DB09845D8 (void);
+// 0x00000009 System.Collections.IEnumerator FlappyBirdController::AutoFlap()
+extern void FlappyBirdController_AutoFlap_m235151D4D8565FB38E9676A40621704A9B872FD7 (void);
+// 0x0000000A System.Void FlappyBirdController::Pause(System.Boolean)
+extern void FlappyBirdController_Pause_m7AB948C44FECF394FAC346992A3A9216CCBD3E82 (void);
+// 0x0000000B System.Void FlappyBirdController::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void FlappyBirdController_OnCollisionEnter2D_m2B053E9D1734405D063423EAE6030C1E8E4B47ED (void);
+// 0x0000000C System.Void FlappyBirdController::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void FlappyBirdController_OnTriggerEnter2D_mB5B9BD2DED53F5B172E4DB08C4DD9578C2898408 (void);
+// 0x0000000D System.Void FlappyBirdController::Die()
+extern void FlappyBirdController_Die_mAFC2B9AD9A320F5369C06A4640AB381889FBB5AA (void);
+// 0x0000000E System.Void FlappyBirdController::.ctor()
+extern void FlappyBirdController__ctor_m6CA82B9583FE8408768F9B35ECE17D53F326FA69 (void);
+// 0x0000000F System.Void FlappyBirdController/<AutoFlap>d__13::.ctor(System.Int32)
+extern void U3CAutoFlapU3Ed__13__ctor_m5B22AB2923909EF706ED5831D3575999960EB306 (void);
+// 0x00000010 System.Void FlappyBirdController/<AutoFlap>d__13::System.IDisposable.Dispose()
+extern void U3CAutoFlapU3Ed__13_System_IDisposable_Dispose_mCD41568A271EF0388B184E1178991040C636CA9E (void);
+// 0x00000011 System.Boolean FlappyBirdController/<AutoFlap>d__13::MoveNext()
+extern void U3CAutoFlapU3Ed__13_MoveNext_mEB4AA65F3568EC26257584FEB96B92F7B83034A4 (void);
+// 0x00000012 System.Object FlappyBirdController/<AutoFlap>d__13::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CAutoFlapU3Ed__13_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m9DED86C2C95D8956FAD1DDA6D04F139659DEDD3A (void);
+// 0x00000013 System.Void FlappyBirdController/<AutoFlap>d__13::System.Collections.IEnumerator.Reset()
+extern void U3CAutoFlapU3Ed__13_System_Collections_IEnumerator_Reset_mF5741460C3B7E458D6A1C0755C29E03ACE3D0372 (void);
+// 0x00000014 System.Object FlappyBirdController/<AutoFlap>d__13::System.Collections.IEnumerator.get_Current()
+extern void U3CAutoFlapU3Ed__13_System_Collections_IEnumerator_get_Current_m7B5BD4ECC0DD8C0EA9BC102AFF94EF3285153B91 (void);
+// 0x00000015 System.Int32 Game_Manager::get_BestScore()
+extern void Game_Manager_get_BestScore_m5D8CBC60AC6E3311011EDF492219A05F31E88EA2 (void);
+// 0x00000016 System.Void Game_Manager::set_BestScore(System.Int32)
+extern void Game_Manager_set_BestScore_mFAFDB031A9D6DBD023B5773523CFB56C50355DC7 (void);
+// 0x00000017 System.Boolean Game_Manager::get_IsPaused()
+extern void Game_Manager_get_IsPaused_mD5285CBA57D7B59401B82BBA4160FD684DC3179B (void);
+// 0x00000018 System.Void Game_Manager::set_IsPaused(System.Boolean)
+extern void Game_Manager_set_IsPaused_mB897EEA3DF5A54AC87210D5E11E41B9775D14513 (void);
+// 0x00000019 System.Boolean Game_Manager::get_IsGameStarted()
+extern void Game_Manager_get_IsGameStarted_mB3745CAC3D08461DC9AFF735658633E16E15D134 (void);
+// 0x0000001A System.Void Game_Manager::set_IsGameStarted(System.Boolean)
+extern void Game_Manager_set_IsGameStarted_mA47E4BE33CBA3AD790E769671B862B5F4834E9CE (void);
+// 0x0000001B System.Void Game_Manager::Awake()
+extern void Game_Manager_Awake_m47E7E11D25A7F6AE2627C0624C408048E966241F (void);
+// 0x0000001C System.Void Game_Manager::Home()
+extern void Game_Manager_Home_mA523146DF6973583562E4AAFBC2316B67C2C1DA8 (void);
+// 0x0000001D System.Void Game_Manager::StartGame()
+extern void Game_Manager_StartGame_mCB9CB5B8D73AD36AE9869585F81F16E28687F7F8 (void);
+// 0x0000001E System.Void Game_Manager::AddScorePoint()
+extern void Game_Manager_AddScorePoint_m0DD49D2BF5128C1CF1942C77D82E47BA5564740B (void);
+// 0x0000001F System.Void Game_Manager::Pause()
+extern void Game_Manager_Pause_m8CAA56006F0E8D0EB0BB5A65B2A3A257E20170CB (void);
+// 0x00000020 System.Void Game_Manager::Die()
+extern void Game_Manager_Die_m346F46DFB8C9E73D845E485597191C96DFB1DC72 (void);
+// 0x00000021 System.Void Game_Manager::Exit()
+extern void Game_Manager_Exit_mE00C6008F49C9D6A340D33B893427C82908AF16E (void);
+// 0x00000022 System.Void Game_Manager::.ctor()
+extern void Game_Manager__ctor_m9106ED26DC519A90AF5F15D0CA9FC23FFA0AF4A6 (void);
+// 0x00000023 System.Void GenerateScenario::Awake()
+extern void GenerateScenario_Awake_mF909D1CBC00A8F40B543B09EE233620A5A6E2D11 (void);
+// 0x00000024 System.Void GenerateScenario::Update()
+extern void GenerateScenario_Update_m38F3DCBE1D30A050DAA09FD212B02F7ADB76F16B (void);
+// 0x00000025 System.Void GenerateScenario::CleanUsedBackgrounds()
+extern void GenerateScenario_CleanUsedBackgrounds_m0CE0E82B7BE66226F0B8DC3A2DF8F2CFEF015422 (void);
+// 0x00000026 System.Void GenerateScenario::CleanAllBackground()
+extern void GenerateScenario_CleanAllBackground_m904D4DA213B114ABA4668E69173D231625AB9C1F (void);
+// 0x00000027 System.Void GenerateScenario::.ctor()
+extern void GenerateScenario__ctor_m6BE8FFEF11F599E7FDF4D97F9F4327AE14A59A1A (void);
+// 0x00000028 System.Void PlayAudioOnCollisionEnter::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void PlayAudioOnCollisionEnter_OnCollisionEnter2D_m41A2DD50F86BB1A6C6FF27997C23A364E9888ECE (void);
+// 0x00000029 System.Void PlayAudioOnCollisionEnter::.ctor()
+extern void PlayAudioOnCollisionEnter__ctor_mF1CDEA07034FACC8D603DECDB536A3697CC4D8AF (void);
+// 0x0000002A System.Void PlayAudioOnTriggerEnter::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void PlayAudioOnTriggerEnter_OnTriggerEnter2D_m6D753F022407E543FB03921A8C28703D9CAED23F (void);
+// 0x0000002B System.Void PlayAudioOnTriggerEnter::.ctor()
+extern void PlayAudioOnTriggerEnter__ctor_m7F7B0BBD8E3F60119A52413FD56644C2952D82BE (void);
+// 0x0000002C System.Void Settings_Manager::TurnOnSettingMenu()
+extern void Settings_Manager_TurnOnSettingMenu_m2C3D3A90BA953460C71755A7F0AD11A91EDDE2ED (void);
+// 0x0000002D System.Void Settings_Manager::SoundOn()
+extern void Settings_Manager_SoundOn_mC99DDAECB958E580C83E85300C668671B36C6CF9 (void);
+// 0x0000002E System.Void Settings_Manager::SoundOff()
+extern void Settings_Manager_SoundOff_mDF587D0590500E642711EE9976A91A48A30A5E43 (void);
+// 0x0000002F System.Void Settings_Manager::.ctor()
+extern void Settings_Manager__ctor_m4C5D69FF3A2ADDADF5003FEE60702EFC729DAEC0 (void);
+static Il2CppMethodPointer s_methodPointers[47] = 
+{
+	DeadCanvas_Manager_SetScoreValues_m5EADA471067A78758E39D0FCC1D538552B621700,
+	DeadCanvas_Manager__ctor_m18561F35BDB84ACE19FD979A1BD4E1BDC5B7D781,
+	FlappyBirdController_get_IsDead_m674AE80FE8EBC7894231F02A0463120DAB9AE64D,
+	FlappyBirdController_set_IsDead_m759BC84B979C4235D4BDB8F9FB68C277F30AB847,
+	FlappyBirdController_Start_mFEE39692CA8EBDF3572319E6AE9EF975010BFC23,
+	FlappyBirdController_Update_m45F9A45A2010AE6AC8C4FB4B9020F2F8F8FCDE70,
+	FlappyBirdController_Flap_m057D63348E1809B9DBF82942FEC89F628F60560E,
+	FlappyBirdController_MoveCameraForward_m7F064934739EDF67DF1C1038D5E80B1DB09845D8,
+	FlappyBirdController_AutoFlap_m235151D4D8565FB38E9676A40621704A9B872FD7,
+	FlappyBirdController_Pause_m7AB948C44FECF394FAC346992A3A9216CCBD3E82,
+	FlappyBirdController_OnCollisionEnter2D_m2B053E9D1734405D063423EAE6030C1E8E4B47ED,
+	FlappyBirdController_OnTriggerEnter2D_mB5B9BD2DED53F5B172E4DB08C4DD9578C2898408,
+	FlappyBirdController_Die_mAFC2B9AD9A320F5369C06A4640AB381889FBB5AA,
+	FlappyBirdController__ctor_m6CA82B9583FE8408768F9B35ECE17D53F326FA69,
+	U3CAutoFlapU3Ed__13__ctor_m5B22AB2923909EF706ED5831D3575999960EB306,
+	U3CAutoFlapU3Ed__13_System_IDisposable_Dispose_mCD41568A271EF0388B184E1178991040C636CA9E,
+	U3CAutoFlapU3Ed__13_MoveNext_mEB4AA65F3568EC26257584FEB96B92F7B83034A4,
+	U3CAutoFlapU3Ed__13_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m9DED86C2C95D8956FAD1DDA6D04F139659DEDD3A,
+	U3CAutoFlapU3Ed__13_System_Collections_IEnumerator_Reset_mF5741460C3B7E458D6A1C0755C29E03ACE3D0372,
+	U3CAutoFlapU3Ed__13_System_Collections_IEnumerator_get_Current_m7B5BD4ECC0DD8C0EA9BC102AFF94EF3285153B91,
+	Game_Manager_get_BestScore_m5D8CBC60AC6E3311011EDF492219A05F31E88EA2,
+	Game_Manager_set_BestScore_mFAFDB031A9D6DBD023B5773523CFB56C50355DC7,
+	Game_Manager_get_IsPaused_mD5285CBA57D7B59401B82BBA4160FD684DC3179B,
+	Game_Manager_set_IsPaused_mB897EEA3DF5A54AC87210D5E11E41B9775D14513,
+	Game_Manager_get_IsGameStarted_mB3745CAC3D08461DC9AFF735658633E16E15D134,
+	Game_Manager_set_IsGameStarted_mA47E4BE33CBA3AD790E769671B862B5F4834E9CE,
+	Game_Manager_Awake_m47E7E11D25A7F6AE2627C0624C408048E966241F,
+	Game_Manager_Home_mA523146DF6973583562E4AAFBC2316B67C2C1DA8,
+	Game_Manager_StartGame_mCB9CB5B8D73AD36AE9869585F81F16E28687F7F8,
+	Game_Manager_AddScorePoint_m0DD49D2BF5128C1CF1942C77D82E47BA5564740B,
+	Game_Manager_Pause_m8CAA56006F0E8D0EB0BB5A65B2A3A257E20170CB,
+	Game_Manager_Die_m346F46DFB8C9E73D845E485597191C96DFB1DC72,
+	Game_Manager_Exit_mE00C6008F49C9D6A340D33B893427C82908AF16E,
+	Game_Manager__ctor_m9106ED26DC519A90AF5F15D0CA9FC23FFA0AF4A6,
+	GenerateScenario_Awake_mF909D1CBC00A8F40B543B09EE233620A5A6E2D11,
+	GenerateScenario_Update_m38F3DCBE1D30A050DAA09FD212B02F7ADB76F16B,
+	GenerateScenario_CleanUsedBackgrounds_m0CE0E82B7BE66226F0B8DC3A2DF8F2CFEF015422,
+	GenerateScenario_CleanAllBackground_m904D4DA213B114ABA4668E69173D231625AB9C1F,
+	GenerateScenario__ctor_m6BE8FFEF11F599E7FDF4D97F9F4327AE14A59A1A,
+	PlayAudioOnCollisionEnter_OnCollisionEnter2D_m41A2DD50F86BB1A6C6FF27997C23A364E9888ECE,
+	PlayAudioOnCollisionEnter__ctor_mF1CDEA07034FACC8D603DECDB536A3697CC4D8AF,
+	PlayAudioOnTriggerEnter_OnTriggerEnter2D_m6D753F022407E543FB03921A8C28703D9CAED23F,
+	PlayAudioOnTriggerEnter__ctor_m7F7B0BBD8E3F60119A52413FD56644C2952D82BE,
+	Settings_Manager_TurnOnSettingMenu_m2C3D3A90BA953460C71755A7F0AD11A91EDDE2ED,
+	Settings_Manager_SoundOn_mC99DDAECB958E580C83E85300C668671B36C6CF9,
+	Settings_Manager_SoundOff_mDF587D0590500E642711EE9976A91A48A30A5E43,
+	Settings_Manager__ctor_m4C5D69FF3A2ADDADF5003FEE60702EFC729DAEC0,
+};
+static const int32_t s_InvokerIndices[47] = 
+{
+	1353,
+	3206,
+	3070,
+	2569,
+	3206,
+	3206,
+	3206,
+	3206,
+	3125,
+	2569,
+	2623,
+	2623,
+	3206,
+	3206,
+	2607,
+	3206,
+	3070,
+	3125,
+	3206,
+	3125,
+	3107,
+	2607,
+	4901,
+	4838,
+	3070,
+	2569,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+	2623,
+	3206,
+	2623,
+	3206,
+	3206,
+	3206,
+	3206,
+	3206,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	47,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
