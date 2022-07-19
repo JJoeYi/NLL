7b041f503b09e7fc68442c53a686d5c0e3cb18c8
LimZhi <107316361+LimZhi@users.noreply.github.com>
2022-07-19 18:54:00 +0800

    Update LivePreviewActivityMain.java

--- app/src/main/java/com/orbitalnll/nll/LivePreviewActivityMain.java
+++ app/src/main/java/com/orbitalnll/nll/LivePreviewActivityMain.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 Google LLC. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.orbitalnll.nll;
 
 import android.Manifest;
@@ -63,6 +47,7 @@ public final class LivePreviewActivityMain extends AppCompatActivity
     private static final String PUSH_UP = "Push Up";
     private static final String SQUAT = " Squat";
     private static final String SIT_UP = "Sit Up";
+    private static final String LUNGES = "Lunges";
 
 
     private CameraSource cameraSource = null;
@@ -116,6 +101,7 @@ public final class LivePreviewActivityMain extends AppCompatActivity
         exerciseList.add(PUSH_UP);
         exerciseList.add(SQUAT);
         exerciseList.add(SIT_UP);
+        exerciseList.add(LUNGES);
 
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, R.layout.spinner_style, exerciseList);
